588d1cd679477b6fa1a9a43802ffacbf834a1173
Francisco Herrera <fhercar3008@g.educaand.es>
2026-09-23 17:01:58 +0200

    Tarea 23/09/2026 subida

---
+++ practica1.py
@@ -0,0 +1,29 @@
+def main():
+    
+    lista = ["Manzana", "Pera", "Melocoton"]
+    lista2 = ["Kiwi", "Sandia", "Melon"]
+    
+    lista.extends(lista2)
+    print(lista[-1])  # -1 Siempre te dice el último
+    
+    tupla = (3,5,7)
+    print(tupla[0])
+    
+    inicio = int(input("Inicio:"))
+    fin = int(input("Fin:"))
+    salto = int(input("Salto:"))
+    
+    rango = range(inicio,fin,salto)
+    print(rango)
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
+
+
+
+
+
